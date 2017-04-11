6587ca747e577ffc2d66a08425eaeb0c32f42aa5
alipman95 <alipman@udel.edu>
2017-04-11 13:34:37 -0400

    Added rule header and changed plugin name

--- Code/sonar-example-plugin/src/main/java/MyFirstCustomCheck.java
+++ Code/sonar-example-plugin/src/main/java/MyFirstCustomCheck.java
@@ -3,6 +3,7 @@
 import com.google.common.collect.ImmutableList;
 
 import org.sonar.check.Rule;
+import org.sonar.check.Priority;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.semantic.Symbol.MethodSymbol;
 import org.sonar.plugins.java.api.semantic.Type;
@@ -12,7 +13,12 @@ import org.sonar.plugins.java.api.tree.Tree.Kind;
 
 import java.util.List;
 
-@Rule(key = "MyFirstCustomRule")
+@Rule(
+		  key = "MyFirstCustomCheck",
+		  name = "Return type and parameter of a method should not be the same",
+		  description = "For a method having a single parameter, the types of its return value and its parameter should never be the same.",
+		  priority = Priority.CRITICAL,
+		  tags = {"bug"})
 public class MyFirstCustomCheck extends IssuableSubscriptionVisitor {
 
 	@Override
